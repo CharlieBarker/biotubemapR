832306b906b83815db96079c60faab5fac64053e
CharlieBarker <zcbtark@ucl.ac.uk>
2024-09-11 10:27:21 +0100

    add colour scheme

---
+++ R/tfl_color_scheme.R
@@ -0,0 +1,19 @@
+# Define the color palette
+london_underground_palette <- c(
+  "Bakerloo" = "#B36305",
+  "Central" = "#E32017",
+  "Circle" = "#FFD300",
+  "District" = "#00782A",
+  "Elizabeth" = "#6950a1",
+  "Hammersmith & City" = "#F3A9BB",
+  "Jubilee" = "#A0A5A9",
+  "Metropolitan" = "#9B0056",
+  "Northern" = "#000000",
+  "Piccadilly" = "#003688",
+  "Victoria" = "#0098D4",
+  "Waterloo & City" = "#95CDBA",
+  "DLR" = "#00A4A7",
+  "London Overground" = "#EE7C0E",
+  "London Trams" = "#84B817",
+  "Emirates Cable Car" = "#E21836"
+)
